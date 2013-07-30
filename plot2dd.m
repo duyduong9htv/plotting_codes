b9815d97e60ba3e6a07e9ac247ec17a68c6a5233
DD Tran <duyduong9htv@yahoo.com>
2013-07-29 23:33:39 -0400

    add plot Nx2 function

---
+++ plot2dd.m
@@ -0,0 +1,6 @@
+function h = plot2dd(x, varargin)
+%function plot2dd(x, varargin) 
+%takes in the Nx2 data x and plot. Save typing time. 
+% Eg. x = rand(10, 2); 
+% plot2dd(x, '*', 'color', 'r', 'markersize', 5); 
+h = plot(x(:, 1), x(:, 2), varargin{:}); 
